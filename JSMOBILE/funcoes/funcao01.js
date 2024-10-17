6286ef861759799bf8cd7eae81151dd696e41ab5
sanmaryh <marianne.marisanches@gmail.com>
2024-10-17 11:07:18 -0300

    Js mobile

---
+++ JSMOBILE/funcoes/funcao01.js
@@ -0,0 +1,22 @@
+//Atividades funções Javascript
+// 1) Funçao soma(n1,n2)
+// 2) Funçao soma(n1,n2) com checagem if/else
+// 3 funçao nomeAltura(nome,altura)com checagem if/else
+// 4) funçao numeroParOuImpar(n1)
+// 5) funçao numeroParOuImpar(n1) com checagem if/else
+
+function dados1(nome,altura) {
+  console.log("Olá" + nome + "sua altura é" + altura)
+}
+
+fuction soma1(n1,n2){
+  cont res = n1 + n2
+  console.log("resultado: "+re)
+}
+//soma1("abobora",20)
+
+fuction soma2(n1,n2){
+  if(type n1 === "number" && typeof n2 ==="number") {
+  cont res = n1 + n2
+  console.log("resultado: "+re)
+}else {console.log("erro")}o}
