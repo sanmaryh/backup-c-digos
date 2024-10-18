113f0f8f1c0cfd1e5df4462f1a6e92538e63e2ca
sanmaryh <marianne.marisanches@gmail.com>
2024-10-18 19:14:54 -0300

    introdução JS

---
+++ Intoducao-JS/script.js
@@ -0,0 +1,12 @@
+const idade = "19"
+const nome = "Beatriz"
+const sobrenome = "fachones"
+const telefone = "41 995349190"
+const estaCasado = false
+
+
+console.log(idade)
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estaCasado)
